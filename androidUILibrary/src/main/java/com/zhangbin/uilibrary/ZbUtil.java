e5c1120d2d9b2f9248ed2a2c48ce83340c063cd8
zhangbin <zhangbinwork@dingtalk.com>
2022-07-13 16:51:10 +0800

    Initial commit

---
+++ androidUILibrary/src/main/java/com/zhangbin/uilibrary/ZbUtil.java
@@ -0,0 +1,15 @@
+package com.zhangbin.uilibrary;
+
+/**
+ * User: ZB
+ * Email: 313441157@qq.com
+ * Date: 2022/7/13
+ * Description:
+ */
+public class ZbUtil {
+
+    public static String getHelloWorld(){
+        return "HelloWorld";
+    }
+
+}
